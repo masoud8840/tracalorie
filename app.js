32b26a764022710b87207e7291260471c30e112e
Masoud Gharedaghi <masoudgharedaghi@yahoo.com>
2024-04-04 16:25:02 +0330

    security

--- app.js
+++ app.js
@@ -2,6 +2,11 @@ const express = require("express");
 const dotenv = require("dotenv");
 const colors = require("colors");
 const cors = require("cors");
+const mongoSanitize = require("express-mongo-sanitize");
+const helmet = require("helmet");
+const xss = require("xss-clean");
+const rateLimit = require("express-rate-limit");
+const hpp = require("hpp");
 
 // Routes file
 const auth = require("./routes/Auth.js");
@@ -17,6 +22,12 @@ connectToDB();
 const app = express();
 app.use(express.json());
 app.use(cors());
+app.use(mongoSanitize());
+app.use(helmet());
+app.use(xss());
+const rateLimiter = rateLimit({ windowMs: 1000, max: 2 });
+app.use(rateLimiter);
+app.use(hpp());
 
 app.use("/api/v1/auth", auth);
 app.use("/api/v1/meals", meals);
